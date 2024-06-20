f8ea20b602855bd2be3a53ada201af02f9ba2e25
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2024-06-20 11:12:39 -0700

    convertion scripts

---
+++ scripts/run_dbem/conversion_protocol.R
@@ -0,0 +1,36 @@
+# Settings file for converting DBEM
+# .txt files to .Rdata files
+# Juliano Palacios
+
+# Load required functions
+library(here)
+library(tidyverse)
+
+# Variables to be converted (Abundace or Catch)
+variables <- c("Abd")
+
+# Partial fix for not working species
+# spplist <- dplyr::filter(spplist, V1 %in% c("603310","604708"))
+
+# Determine the start and end year you want to include
+stryr <- 1851
+endyr <- 2100
+
+# Scenario to call (Note this will determine the results directory)
+scen <- "c6gfdl26f1mpanow"
+
+# Include here the path of your DBEM raw outputs BEFORE the scenario
+dbem_outputs <- list.files(paste0("",scen),full.names = T)
+
+# Load required functions
+# source(here("support_fx/convert_fx.R")) # Load a bunch of packages
+source("/home/jepa/projects/def-wailung/dbem/support_fx/convert_fx.R")
+
+# Call function for scenarios in Settings file
+lapply(variables,
+       convert_fx,
+       year_one = stryr,
+       year_end = endyr,
+       scen = scen,
+       csv_path = dbem_outputs
+)
---
+++ scripts/run_dbem/run_conversion.sh
@@ -0,0 +1,22 @@
+#!/bin/bash                                                                     
+#SBATCH --job-name=ADRD_R                                                       
+#SBATCH --account=def-wailung                                                   
+#SBATCH --nodes=1 # number of node MUST be 1                                    
+#SBATCH --ntasks=1                                                              
+#SBATCH --cpus-per-task=1        # number of processes                          
+#SBATCH --mem=1G                                                                
+#SBATCH -t 00-00:20:00                                                         
+#SBATCH --mail-user=j.palacios@oceans.ubc.ca                                    
+#SBATCH --mail-type=ALL                                                         
+#SBATCH --output=/home/jepa/projects/def-wailung/jepa/mpa_cc_toe/scripts/run_dbem/slurm_out/con_slurm_%j.out # Specify the full path with the desired file name prefix
+
+
+# ---------------------------------------------------------------------         
+echo "Current working directory: `pwd`"
+echo "Starting run at: `date`"
+# ---------------------------------------------------------------------         
+
+
+module load StdEnv/2023 gcc/12.3 r/4.3.1
+export R_LIBS=~/local/R_libs/
+Rscript conversion_protocol.R Settings.R$SLURM_ARRAY_TASK_ID
